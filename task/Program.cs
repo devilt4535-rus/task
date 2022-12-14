26fe8447f23103ba8602769adee425f97d773b77
Chef <Chef@DESKTOP-MLH0HHL>
2022-12-14 23:46:18 +0300

    Добавьте файлы проекта.

---
+++ task/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+         Numder number = new();
+        int[] arrayNumbers = new int[] { 2, 2, 2, 1, 2, 2, 2, 2, 2 };
+        number.FindUniqueNumberArray(arrayNumbers);
+    }
+}
+
---
+++ task/nums.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task
+{
+    internal class nums
+    {
+        public void FindUniqueNumberArray(int[] arrayNumbers)
+        {
+            if (arrayNumbers.Length % 2 != 0)
+            {
+                for (int i = 0; i < arrayNumbers.Length; i++)
+                {
+                    int count = 0;
+                    int numberInArray = arrayNumbers[i];
+                    foreach (var number in arrayNumbers)
+                    {
+                        if (number == numberInArray)
+                        {
+                            count++;
+                        }
+                    }
+                    if (count == 1)
+                    {
+                        numberInArray = arrayNumbers[i];
+                        Console.WriteLine();
+                        Console.WriteLine($"Уникальное число из массива - {numberInArray}");
+                        Console.WriteLine();
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Ошибка");
+                Console.WriteLine();
+            }
+        }
+    }
+}
